7ea8de27a8e9672fa5890968455cbc6b41b52db8
Jack The HypeRipper <hyperipper@proton.me>
2026-08-31 20:09:06 +0200

    Fix key mapping in mac-pl-key-fix.sh
    Send output to dev/null as there is no need to print anything

--- mac-pl-key-fix.sh
+++ mac-pl-key-fix.sh
@@ -1,4 +1,4 @@
 hidutil property --set '{"UserKeyMapping":[
   {"HIDKeyboardModifierMappingSrc":0x7000000E7,"HIDKeyboardModifierMappingDst":0x7000000E6},
   {"HIDKeyboardModifierMappingSrc":0x7000000E6,"HIDKeyboardModifierMappingDst":0x7000000E7}
-]}'
+]}' > /dev/null 2>&1
